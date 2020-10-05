6dffa207e84f268ce436e73c52a628ad6db47763
Donald Spini <donald.spini68@gmail.com>
2020-10-05 11:36:23 -0500

    controllers

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,2 +1,34 @@
-package com.codeup.blog;public class PostController {
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    public String indexPage(Model viewModel) {
+        viewModel.addAttribute("posts");
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String viewPost(Model viewModel) {
+        viewModel.addAttribute("post");
+        return "posts/show";
+    }
+
+    @GetMapping("/posts/create")
+    public String viewForm(Model model) {
+        model.addAttribute("post");
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(Model model) {
+        model.addAttribute("post");
+        return "posts/create";
+    }
+
 }
