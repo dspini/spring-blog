074ad2b3a630803e07b57dd86bf8c1387f4f8c65
Donald Spini <donald.spini68@gmail.com>
2020-10-07 12:11:46 -0500

    repository and JPA

--- src/main/java/com/codeup/blog/repositories/AdRepository.java
+++ src/main/java/com/codeup/blog/repositories/AdRepository.java
@@ -8,11 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AdRepository extends JpaRepository<Post.Ad, Long> {
-        // The following method is equivalent to the built in `getOne` method, there's no need to create this example
+
         @Query("from Ad a where a.id like ?1")
         Post.Ad getAdById(long id);
 
-        // The following method shows you how to use named parameters in a HQL custom query:
         @Query("from Ad a where a.title like %:term%")
         List<Post.Ad> searchByTitleLike(@Param("term") String term);
     }
