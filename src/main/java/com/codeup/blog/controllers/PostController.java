419504fe72cd547090ef119ab7f0b0f2462eb15d
Donald Spini <donald.spini68@gmail.com>
2020-10-08 11:28:06 -0500

    repository and JPA

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -2,6 +2,9 @@ package com.codeup.blog.controllers;
 
 import com.codeup.blog.models.Post;
 import com.codeup.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PostController {
     private final PostRepository postRepo;
+    private JpaRepository postsRepo;
 
-    public PostController(PostRepository postRepo) {
+    public PostController(PostRepository postRepo, @Qualifier(" ") JpaRepository postsRepo) {
         this.postRepo = postRepo;
+        this.postsRepo = postsRepo;
     }
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
@@ -38,4 +43,26 @@ public class PostController {
     public String sendingCreatedPostToDB() {
         return "Sending your diary page to be seen by everyone. :)";
     }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model){
+        model.addAttribute("post", postsRepo.getOne(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String editPost(@PathVariable long id, @ModelAttribute Post post){
+        JpaRepository usersRepo = null;
+        SecurityProperties.User user = (SecurityProperties.User) usersRepo.getOne(1L);
+        post.setAuthor(user);
+        postsRepo.save(post);
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id) {
+        postsRepo.deleteById(id);
+        return "redirect:/posts";
+    }
+
 }
